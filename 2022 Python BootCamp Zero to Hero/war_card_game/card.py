9a516694ec248285e5db7574f85aee7334f37630
Logan T. Meyer <logan.t.meyer@gmail.com>
2022-02-16 08:31:44 -0700

    ups

---
+++ 2022 Python BootCamp Zero to Hero/war_card_game/card.py
@@ -0,0 +1,14 @@
+#CARD
+#SUIT, RANK, VALUE
+
+class Card:
+
+  def __init__(self,suit,rank):
+    self.suit = suit
+    self.rank = rank
+
+    def __str__(self):
+      return self.rank + " of " + self.suit
+
+
+#DECK
